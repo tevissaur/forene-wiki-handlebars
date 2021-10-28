ba7999c58637445e7efc15b9fa964a84cb3b5247
Tevissaur <berserkboar@protonmail.com>
2021-10-27 20:35:50 -0500

    trying to use sass

--- controllers/wikiRoutes/home-routes.js
+++ controllers/wikiRoutes/home-routes.js
@@ -1,16 +1,18 @@
 const router = require('express').Router()
-const { Article, Page } = require('../../models')
+const { Article, Page, Image } = require('../../models')
 
 router.get('/home', async (req, res) => {
     try {
         console.log(Article, Page)
-        const articleData = await Article.findAll([
-            include
-        ])
+        const articleData = await Article.findAll({
+            include: [{ model: Image }]
+        })
         if (!articleData) {
             res.status(404).json({ message: 'No articles found' })
+        } else {
+            const articles = articleData.map( article => article.get({plain: true}))
+            res.render('home', { articles })
         }
-        res.status(200).json(articleData)
     }
     catch (err) {
         res.status(500).json(err)
