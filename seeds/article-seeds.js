7d1852c00dfc6a38427b3389d4772d9788040d5a
Tevissaur <berserkboar@protonmail.com>
2021-10-29 15:32:05 -0500

    finished styling

--- seeds/article-seeds.js
+++ seeds/article-seeds.js
@@ -19,6 +19,42 @@ const articleData = [
         articleText: "Newly declared King of Durglagh, Wendrake Addington, to feast with the Lords of the Torwoods this Festifmar! In a stunning move for the newly crowned statesmen he opts for diplomacy, rather than rile up a ruckus; a peacekeeper, he is. The Emperor has sent his most trusted diplomat, Emile Brack, to aid the green King in his attempt to cool the tensions between him and his former sovereigns, The Lords of the Torwoods. The Treaty of Alderwick will initiate an indefinite truce between the Empire and the Nordlings. Godspeed to Emile, and as always, Godspeed to The Empire!",
         authorAlias: "Historian #1",
         pageId: 1
+    },
+    {
+        title: 'Humans',
+        articleText: "With their penchant for migration and conquest, humans are more physically diverse than other common races. There is no typical human. An individual can stand from 5 feet to a little over 6 feet tall and weigh from 125 to 250 pounds. Human skin shades range from nearly black to very pale, and hair colors from black to blond (curly, kinky, or straight);    males might sport facial hair that is sparse or thick. A lot of humans have a dash of nonhuman blood, revealing hints of elf, orc, or other lineages. Humans reach adulthood in their late teens and rarely live even a single century.",
+        authorAlias: 'Historian #1',
+        pageId: 2
+    },
+    {
+        title: 'Elves',
+        articleText: "With their unearthly grace and fine features, elves appear hauntingly beautiful to humans and members of many other races. They are slightly shorter than humans on average, ranging from well under 5 feet tall to just over 6 feet. They are more slender than humans, weighing only 100 to 145 pounds. Males and females are about the same height, and males are only marginally heavier than females. Elves’ coloration encompasses the normal human range and also includes skin in shades of copper, bronze, and almost bluish-white, hair of green or blue, and eyes like pools of liquid gold or silver. Elves have no facial and little body hair. They favor elegant clothing in bright colors, and they enjoy simple yet lovely jewelry.",
+        authorAlias: 'Historian #1',
+        pageId: 2
+    },
+    {
+        title: 'Dwarves',
+        articleText: "Bold and hardy, dwarves are known as skilled warriors, miners, and workers of stone and metal. Though they stand well under 5 feet tall, dwarves are so broad and compact that they can weigh as much as a human standing nearly two feet taller. Their courage and endurance are also easily a match for any of the larger folk. Dwarven skin ranges from deep brown to a paler hue tinged with red, but the most common shades are light brown or deep tan, like certain tones of earth. Their hair, worn long but in simple styles, is usually black, gray, or brown, though paler dwarves often have red hair. Male dwarves value their beards highly and groom them carefully.",
+        authorAlias: 'Historian #1',
+        pageId: 2
+    },
+    {
+        title: 'Half-Orcs',
+        articleText: "Half-orcs’ grayish pigmentation, sloping foreheads, jutting jaws, prominent teeth, and towering builds make their orcish heritage plain for all to see. Half-orcs stand between 5 and 7 feet tall and usually weigh between 180 and 250 pounds. Orcs regard battle scars as tokens of pride and ornamental scars as things of beauty. Other scars, though, mark an orc or half-orc as a former slave or a disgraced exile. Any half-orc who has lived among or near orcs has scars, whether they are marks of humiliation or of pride, recounting their past exploits and injuries. Such a half-orc living among humans might display these scars proudly or hide them in shame.",
+        authorAlias: 'Historian #1',
+        pageId: 2
+    },
+    {
+        title: 'Gnomes',
+        articleText: "A gnome’s energy and enthusiasm for living shines through every inch of his or her tiny body. Gnomes average slightly over 3 feet tall and weigh 40 to 45 pounds. Their tan or brown faces are usually adorned with broad smiles (beneath their prodigious noses), and their bright eyes shine with excitement. Their fair hair has a tendency to stick out in every direction, as if expressing the gnome’s insatiable interest in everything around. A gnome’s personality is writ large in his or her appearance. A male gnome’s beard, in contrast to his wild hair, is kept carefully trimmed but often styled into curious forks or neat points. A gnome’s clothing, though usually made in modest earth tones, is elaborately decorated with embroidery, embossing, or gleaming jewels.",
+        authorAlias: 'Historian #1',
+        pageId: 2
+    },
+    {
+      title: "Halflings",
+      articleText: "The diminutive halflings survive in a world full of larger creatures by avoiding notice or, barring that, avoiding offense. Standing about 3 feet tall, they appear relatively harmless and so have managed to survive for centuries in the shadow of empires and on the edges of wars and political strife. They are inclined to be stout, weighing between 40 and 45 pounds. Halflings’ skin ranges from tan to pale with a ruddy cast, and their hair is usually brown or sandy brown and wavy. They have brown or hazel eyes. Halfling men often sport long sideburns, but beards are rare among them and mustaches even more so. They like to wear simple, comfortable, and practical clothes, favoring bright colors. Halfling practicality extends beyond their clothing. They’re concerned with basic needs and simple pleasures and have little use for ostentation. Even the wealthiest of halflings keep their treasures locked in a cellar rather than on display for all to see. They have a knack for finding the most straightforward solution to a problem, and have little patience for dithering.",
+      authorAlias: 'Historian #1',
+      pageId: 2  
     }
 ]
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const session = require('express-session');
 const exphbs = require('express-handlebars')
+const helpers = require('./utils/helpers')
 const routes = require('./controllers')
 const sequelize = require('./config/connection')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -13,27 +14,27 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers })
 
 const sess = {
-    // make the secret in an env file and make it secure
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+  // make the secret in an env file and make it secure
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-  app.use(session(sess))
+app.use(session(sess))
 
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
 app.use(express.json())
-app.use (express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+  app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
 })
