b64c9448c0f8bb2fc70436aa51b0f77c68ceaa19
Tevissaur <berserkboar@protonmail.com>
2021-10-29 22:04:16 -0500

    added a view

--- controllers/wikiRoutes/home-routes.js
+++ controllers/wikiRoutes/home-routes.js
@@ -3,8 +3,10 @@ const { Article, Page, Image } = require('../../models')
 
 router.get('/home', async (req, res) => {
     try {
-        console.log(Article, Page)
         const articleData = await Article.findAll({
+            where: {
+                pageId: 1
+            },
             include: [{ model: Image }]
         })
         if (!articleData) {
@@ -12,6 +14,25 @@ router.get('/home', async (req, res) => {
         } else {
             const articles = articleData.map( article => article.get({plain: true}))
             res.render('home', { articles })
+        }
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+router.get('/races', async (req, res) => {
+    try {
+        const articleData = await Article.findAll({
+            where: {
+                pageId: 2
+            },
+            include: [{ model: Image }]
+        })
+        if (!articleData) {
+            res.status(404).json({ message: 'No articles found' })
+        } else {
+            const articles = articleData.map( article => article.get({plain: true}))
+            res.render('races', { articles })
         }
     }
     catch (err) {
